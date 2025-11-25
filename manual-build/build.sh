519f9d8a8c38b1c2e9769f4a470b3dd87b94fda9
TheBurb <twentyfiveex+theancient@gmail.com>
2025-11-24 19:48:27 -0700

    missing close quotes

--- manual-build/build.sh
+++ manual-build/build.sh
@@ -3,4 +3,4 @@
 IMAGE_TAG="$(git rev-parse --short=7 HEAD)"
 IMAGE_NAME="theburb/theancient"
 docker build -t "${IMAGE_NAME}:latest" -f dockerfiles/Dockerfile .
-docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${IMAGE_TAG}
+docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${IMAGE_TAG}"
